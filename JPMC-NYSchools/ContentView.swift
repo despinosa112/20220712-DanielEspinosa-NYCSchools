06998a702e2551a100806cf6e6116a703d5ddcf2
Daniel Espinosa <despinosa112@gmail.com>
2022-07-09 12:40:17 -0400

    Initial Commit

---
+++ JPMC-NYSchools/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JPMC-NYSchools
+//
+//  Created by Daniel Espinosa on 7/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JPMC-NYSchools/JPMC_NYSchoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPMC_NYSchoolsApp.swift
+//  JPMC-NYSchools
+//
+//  Created by Daniel Espinosa on 7/9/22.
+//
+
+import SwiftUI
+
+@main
+struct JPMC_NYSchoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
